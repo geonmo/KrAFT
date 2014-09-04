710fdda3914aacd5c8e1ff49f95c6f37a2768651
Geonmo <ry840901@gmail.com>
2014-09-04 15:53:30 +0900

    bugfix about z boson.

--- RecoSelectorTools/python/zSelector_cfi.py
+++ RecoSelectorTools/python/zSelector_cfi.py
@@ -1,13 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
 ZToMuMuCandidates = cms.EDProducer("CandViewCombiner",
-    decay = cms.string("goodMuons@+ goodMuons@-"),
+    decay = cms.string("flatMuons@+ flatMuons@-"),
     cut = cms.string("86.0 < mass < 96.0"),
     name = cms.string("ZToMuMuCandidates"),
     roles = cms.vstring('muon1','muon2'),
 )
 ZToElElCandidates = cms.EDProducer("CandViewCombiner",
-    decay = cms.string("goodElectrons@+ goodElectrons@-"),
+    decay = cms.string("flatElectrons@+ flatElectrons@-"),
     cut = cms.string("86.0 < mass < 96.0"),
     name = cms.string("ZToElElCandidates"),
     roles = cms.vstring('electron1','electron2'),
